1bb27d9faa968f7b874843c72c83c3dda1b759b2
epecho <dan.epecho@gmail.com>
2017-05-16 17:56:16 -0400

    Finished

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,22 @@
+// // Set up MySQL connection.
+// var mysql = require("mysql");
+// var connection;
+//
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSBB_URL);
+//   }else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "Mota1234",
+//     database: "burgers_db"
+//   });
+//
+// };
+
+
+
+
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
@@ -6,13 +25,17 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSBB_URL);
   }else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Mota1234",
-    database: "burgers_db"
+    host: "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "fdl9oinqv9nblq0y",
+    password: "hzmmd02n2vi9pnrb",
+    database: "qx9dnksnlkrjgy2h"
   });
 
 };
+
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
